9fbbf58bb46862968fec599c3831976dd1c875fe
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-14 02:14:11 +0200

    Update types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,17 +1,22 @@
 export type School = "Ingegneria" | "Architettura" | "Design" | "Urbanistica"
 
 export type TableData = (string | number)[][]
+
+export type Course = {
+  name: string
+  table: TableData
+}
+
+type Phase = {
+  phase: string
+  global: TableData
+  courses: Course[]
+}
+
 export type Structure = {
   school: School
   years: {
     year: number
-    phases: {
-      phase: string
-      global: TableData
-      courses: {
-        name: string
-        table: TableData
-      }[]
-    }[]
+    phases: Phase[]
   }[]
 }[]
